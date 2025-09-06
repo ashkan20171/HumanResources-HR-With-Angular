128e35bae6ce9311cd0e69bc66cfc553776cc8bc
ashkan20171 <ashkan.mataee@yahoo.com>
2025-09-06 16:03:49 +0330

    edit sidebar

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   standalone: true,
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    MatListModule,
+  RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css'
 })
