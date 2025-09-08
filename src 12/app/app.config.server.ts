83eec43e8c6feb4709d36130360e661423b33ccb
ashkan20171 <ashkan.mataee@yahoo.com>
2025-09-08 16:19:38 +0330

    edit bills

--- src 12/app/app.config.server.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
-
-export const config: ApplicationConfig = {
-  ...appConfig,
-  providers: [provideServerRendering()]
-};
--- src 12/assets/js/jalaali.js
+++
@@ -1,7 +0,0 @@
-
-/*
- * Jalaali JavaScript Library
- * https://github.com/jalaali/moment-jalaali
- */
-// نسخه کامل باید از گیت‌هاب دریافت شود.
-// در این فایل قرار داده شده است.
