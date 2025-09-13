147094d40ff864d9efb5c1801506d052987b5295
ashkan20171 <ashkan.mataee@yahoo.com>
2025-09-13 09:28:54 +0330

    edit routing

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,24 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "../header/header.component";
-import { SidebarComponent } from "../sidebar/sidebar.component";
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [CommonModule, MatSidenavModule, RouterOutlet, HeaderComponent, SidebarComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent implements OnInit {
-
   constructor(private router: Router) {}
-
   ngOnInit(): void {
     const user = localStorage.getItem('user');
-    if (!user) {
-      this.router.navigate(['/login']);
-    }
+    if (!user) this.router.navigate(['/login']);
   }
 }
